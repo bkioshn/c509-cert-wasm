93eef2121bd35d34e735a2eef7e6f4d161e2887a
bkioshn <bkioshn@gmail.com>
2026-08-28 17:18:04 +0700

    chore: typo
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- guest/modules/c509/src/handlers.rs
+++ guest/modules/c509/src/handlers.rs
@@ -6,16 +6,14 @@ pub fn decode(bytes: Vec<u8>) -> Result<String, String> {
     let cert = C509Certificate::decode(&bytes);
     cert.map_err(|e| e.to_string())
         .and_then(|cert| serde_json::to_string(&cert).map_err(|e| e.to_string()))
-        .map_err(|e| e.to_string())
 }
 
 /// Decode a C509 certificate sequence from bytes and return a JSON string
-/// Expect a certificate sequnence without a array wrapper with 11 elements.
+/// Expect a certificate sequence without a array wrapper with 11 elements.
 pub fn decode_sequence(bytes: Vec<u8>) -> Result<String, String> {
     let cert = C509Certificate::decode_sequence(&bytes);
     cert.map_err(|e| e.to_string())
         .and_then(|cert| serde_json::to_string(&cert).map_err(|e| e.to_string()))
-        .map_err(|e| e.to_string())
 }
 
 /// Encode a C509 certificate from a JSON string and return bytes
@@ -26,7 +24,7 @@ pub fn encode(json_string: String) -> Result<Vec<u8>, String> {
 }
 
 /// Encode a C509 certificate sequence from a JSON string and return bytes
-/// Return a certificate sequnence without a array wrapper with 11 elements.
+/// Return a certificate sequence without a array wrapper with 11 elements.
 pub fn encode_sequence(json_string: String) -> Result<Vec<u8>, String> {
     let cert: C509Certificate = serde_json::from_str(&json_string).map_err(|e| e.to_string())?;
     Ok(cert.encode_sequence())
